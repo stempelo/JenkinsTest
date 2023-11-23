50b0843eae5be3326dee2044d0b1301fd1b9c2fd
Matteo Boffo aka stempelo <matteo.boffo@gmail.com>
2023-11-23 11:03:39 +0100

    Fix Warning from test

--- src/StringManipulator.php
+++ src/StringManipulator.php
@@ -4,6 +4,9 @@ class StringManipulator
 {
     public function getFirstAndLastCharacter(string $string): array
     {
+        if ( $string === '' ) {
+            return ['',''];
+        }
         $firstCharacter = $string[0];
         $lastCharacter = $string[strlen($string) - 1];
         return [$firstCharacter, $lastCharacter];
